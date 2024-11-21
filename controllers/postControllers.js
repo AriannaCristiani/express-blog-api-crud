0282f1e516a23db4fbd30eebb44a73b348974331
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-11-21 15:44:39 +0100

    aggiunto error 404 sulla funzione destroy

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -15,13 +15,11 @@ function index(req, res) {
             return post.tags.includes(req.query.tag.toLowerCase())
         })
     }
+    console.log(filteredPosts)
 
     const count = filteredPosts.length
 
-    res.json({
-        count,
-        filteredPosts
-    })
+    res.json({ count , filteredPosts})
 }
 
 
@@ -29,6 +27,7 @@ function show(req, res) {
     const id = parseInt(req.params.id)
     //res.send(`questo è il post con id: ${id}`)
     const post = posts.find((post) => post.id === id)
+    console.log(post)
 
     if (!post) {
         res.status(404)
@@ -71,8 +70,10 @@ function update(req, res) {
     //res.send(`stai aggiornando il post con id: ${id}`)
 
     const post = posts.find((post) => post.id === id)
+    console.log(post)
 
     const { title, slug, content, image, tags } = req.body
+    console.log(req.body)
 
     post.title = title;
     post.slug = slug;
@@ -89,8 +90,10 @@ function modify(req, res) {
     //res.send(`stai modificando il post con id: ${id}`)
 
     const post = posts.find((post) => post.id === id)
+    console.log(post)
 
     const { title, slug, content, image, tags } = req.body
+    console.log(req.body)
 
 
     if (title) post.title = title;
@@ -108,6 +111,15 @@ function destroy(req, res) {
     //res.send(`stai eliminando il post con id: ${id}`)
 
     const postIndex = posts.findIndex((post) => post.id === id)
+
+    if (postIndex === -1) {
+		res.status(404)
+
+		return res.json({
+			error: 'Post not found',
+			message: 'Il post non è stato trovato',
+		})
+	}
 
     posts.splice(postIndex, 1)
 
