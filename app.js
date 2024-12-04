1a3eb97e3cf3815e2478f395663c0d62d1736d23
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-12-04 16:25:02 +0100

    installato ed importato cors

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const cors = require('corse')
 const postsRouter = require('./routers/posts.js');
 const findError = require('./middleware/find-error.js');
 const pageNotFound = require('./middleware/page-not-found.js');
 const app = express()
 const port = 3000
+
+//applicazione cors
+app.use(cors());
 
 //visualizzazione immagini
 app.use(express.static('public'));
@@ -14,7 +18,7 @@ app.use(express.json());
 //rotta principale
 app.get('/', (req, res) => {
     //res.send('hello')
-    throw new Error ('Errore di prova') //test errore con middleware
+    throw new Error('Errore di prova') //test errore con middleware
 });
 
 //registrazione del router
